79efaf52508d02c18e9e75d242eee48a4c20b6da
sirpqp <337236126@qq.com>
2021-12-15 22:51:28 +0800

    零件录入前端页面完成

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from settings import *
 
 
@@ -10,6 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("index_1.html")
+
+
+@app.route('/part-input', methods=["GET", "POST"])
+def part_input():
+    if request.method == "GET":
+        return render_template("part-input_1.html")
+
+    if request.method == "POST":
+        args = request.args
 
 
 if __name__ == '__main__':
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,8 @@
 
-$("#main").append(
-    "<div></div>"
-)
+function btnClick() {
+    console.log("点击了此元素")
+}
+
+console.log("加载了index.js文件")
+
+$("#part-input").innerHTML = "JS文件修改了此文本内容!!!"
